3e5ad3644258d0b5f864e206a26cb8268c001783
Anis Ghani <anis9501@gmail.com>
2020-11-25 15:48:39 +0800

    a

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,13 @@
 <?php
 
+use App\Http\Controllers\Auth\EmailVerificationController;
+use App\Http\Controllers\Auth\LogoutController;
+use App\Http\Livewire\Auth\Login;
+use App\Http\Livewire\Auth\Passwords\Confirm;
+use App\Http\Livewire\Auth\Passwords\Email;
+use App\Http\Livewire\Auth\Passwords\Reset;
+use App\Http\Livewire\Auth\Register;
+use App\Http\Livewire\Auth\Verify;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,26 +21,53 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('pages.mainpage');
+Route::view('/', 'welcome'
+    // function(){return redirect()->intended(route('login'));}
+)->name('home');
+
+Route::middleware('guest')->group(function () {
+    Route::get('login', Login::class)
+        ->name('login');
+
+    Route::get('register', Register::class)
+        ->name('register');
 });
 
+Route::get('password/reset', Email::class)
+    ->name('password.request');
+
+Route::get('password/reset/{token}', Reset::class)
+    ->name('password.reset');
+
+Route::middleware('auth')->group(function () {
+    Route::get('email/verify', Verify::class)
+        ->middleware('throttle:6,1')
+        ->name('verification.notice');
+
+    Route::get('password/confirm', Confirm::class)
+        ->name('password.confirm');
+});
+
+Route::middleware('auth')->group(function () {
+    Route::get('email/verify/{id}/{hash}', EmailVerificationController::class)
+        ->middleware('signed')
+        ->name('verification.verify');
+
+    Route::post('logout', LogoutController::class)
+        ->name('logout');
+});
+
+Route::get('/dashboard', function () {
+    return view('pages.mainpage');
+})->name('mainpage');
+
 Route::resource('Emandatelist','\App\Http\Controllers\EmandatelistController');
-
 Route::resource('SenaraiHartaCukai','\App\Http\Controllers\SenaraiHartaCukai');
-
 Route::resource('SenaraiHartaBaru','\App\Http\Controllers\SenaraiHartaBaruController');
-
 Route::resource('ProsesBilCukai','\App\Http\Controllers\ProsesBilCukaiController');
-
 Route::get('DownloadResit','\App\Http\Controllers\ProsesBilCukaiController@downloadBilCukai');
-
 Route::resource('TempahGelanggang','\App\Http\Controllers\TempahGelanggangController');
-
 Route::resource('SenaraiTempahan','\App\Http\Controllers\SenaraiTempahanController');
-
 Route::resource('SenaraiAkaun','\App\Http\Controllers\SenaraiAccountController');
-
 Route::resource('MaklumatAkaunSewa','\App\Http\Controllers\MaklumatAkaunController');
-
 Route::resource('SenaraiKutipan','\App\Http\Controllers\kutipan\SenaraiKutipanController');
