74148b311bf63486f62733468db8251b7e579619
safwan <safwan@csc.net.my>
2020-11-26 16:24:20 +0800

    edit page kutipan

--- resources/views/livewire/kutipan-calculate.blade.php
+++ resources/views/livewire/kutipan-calculate.blade.php
@@ -131,7 +131,7 @@
 
                                             <div class="bg-green-500 hover:bg-grey text-blue-500-darkest font-bold py-2 px-2 rounded-full flex cursor-pointer ml-2" wire:click="addInvoice({{$item1->id}})">
                                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-4 h-4 text-white text-center">
-                                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
                                                 </svg>
                                             </div>
                                         </div>
