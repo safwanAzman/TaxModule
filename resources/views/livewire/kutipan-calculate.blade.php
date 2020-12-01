d1c5283b9d3681547d7d4552d559a4b09d5f6044
safwan <safwan@csc.net.my>
2020-12-01 17:30:19 +0800

    edit kutipan-calculator

--- resources/views/livewire/kutipan-calculate.blade.php
+++ resources/views/livewire/kutipan-calculate.blade.php
@@ -16,81 +16,80 @@
             <div class="bg-white shadow-lg p-4">
                 <div class="flex justify-between">
                     <x-tab.title name="0" livewire="wire:click=chooseCategory('C0')">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-6 h-6 ml-2">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
-                        </svg><span class="tooltip-text bg-blue-500 border rounded border-blue-500 text-white -mt-14">Cukai</span>
+                      <x-heroicon-o-home class="w-6 h-6 ml-2"/>
+                      <span class="tooltip-text bg-blue-500 border rounded border-blue-500 text-white -mt-14">Cukai</span>
                     </x-tab.title>
+
                     <x-tab.title name="1" livewire="wire:click=chooseCategory('L0')">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-6 h-6 ml-2">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 10h18M7 15h1m4 0h1m-7 4h12a3 3 0 003-3V8a3 3 0 00-3-3H6a3 3 0 00-3 3v8a3 3 0 003 3z" />
-                        </svg><span class="tooltip-text bg-blue-500 border rounded border-blue-500 text-white -mt-14">Lesen</span>
+                        <x-heroicon-o-credit-card class="w-6 h-6 ml-2"/>
+                        <span class="tooltip-text bg-blue-500 border rounded border-blue-500 text-white -mt-14">Lesen</span>
                     </x-tab.title>
+
                     <x-tab.title name="2" livewire="wire:click=chooseCategory('S0')">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-6 h-6 ml-2">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 7a2 2 0 012 2m4 0a6 6 0 01-7.743 5.743L11 17H9v2H7v2H4a1 1 0 01-1-1v-2.586a1 1 0 01.293-.707l5.964-5.964A6 6 0 1121 9z" />
-                        </svg><span class="tooltip-text bg-blue-500 border rounded border-blue-500 text-white -mt-14">Sewa</span>
+                        <x-heroicon-o-key class="w-6 h-6 ml-2"/>
+                        <span class="tooltip-text bg-blue-500 border rounded border-blue-500 text-white -mt-14">Sewa</span>
                     </x-tab.title>
             
                     <x-tab.title name="3" livewire="wire:click=chooseCategory('P0')">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-6 h-6 ml-2">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-                        </svg><span class="tooltip-text bg-blue-500 border rounded border-blue-500 text-white -mt-14">Pelbagai</span>
+                        <x-heroicon-o-globe class="w-6 h-6 ml-2"/>
+                       <span class="tooltip-text bg-blue-500 border rounded border-blue-500 text-white -mt-14">Pelbagai</span>
                     </x-tab.title>
+
                     <x-tab.title name="4" livewire="wire:click=chooseCategory('CA')">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-6 h-6 ml-2">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z" />
-                        </svg><span class="tooltip-text bg-blue-500 border rounded border-blue-500 text-white -mt-14">Cagaran</span>
+                        <x-heroicon-o-folder class="w-6 h-6 ml-2"/>
+                        <span class="tooltip-text bg-blue-500 border rounded border-blue-500 text-white -mt-14">Cagaran</span>
                     </x-tab.title>
+
                     <x-tab.title name="5" livewire="wire:click=chooseCategory('KM')">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-6 h-6 ml-2">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 14v3m4-3v3m4-3v3M3 21h18M3 10h18M3 7l9-4 9 4M4 10h16v11H4V10z" />
-                        </svg><span class="tooltip-text bg-blue-500 border rounded border-blue-500 text-white -mt-14">Kompaun MDP</span>
+                        <x-heroicon-o-library class="w-6 h-6 ml-2"/>
+                        <span class="tooltip-text bg-blue-500 border rounded border-blue-500 text-white -mt-14">Kompaun MDP</span>
                     </x-tab.title>
+
                     <x-tab.title name="6" livewire="wire:click=chooseCategory('KS')">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-6 h-6">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 14v3m4-3v3m4-3v3M3 21h18M3 10h18M3 7l9-4 9 4M4 10h16v11H4V10z" />
-                        </svg><span class="tooltip-text bg-blue-500 border rounded border-blue-500 text-white -mt-14">Kompaun Suasa</span>
+                        <x-heroicon-o-library class="w-6 h-6 ml-2"/>
+                        <span class="tooltip-text bg-blue-500 border rounded border-blue-500 text-white -mt-14">Kompaun Suasa</span>
                     </x-tab.title>
+
                     <x-tab.title name="7" livewire="wire:click=chooseCategory('LK')">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-6 h-6 ml-2">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 20l-5.447-2.724A1 1 0 013 16.382V5.618a1 1 0 011.447-.894L9 7m0 13l6-3m-6 3V7m6 10l4.553 2.276A1 1 0 0021 18.382V7.618a1 1 0 00-.553-.894L15 4m0 13V4m0 0L9 7" />
-                        </svg><span class="tooltip-text bg-blue-500 border rounded border-blue-500 text-white -mt-14">Lot Kosong</span>
+                        <x-heroicon-o-map class="w-6 h-6 ml-2"/>
+                        <span class="tooltip-text bg-blue-500 border rounded border-blue-500 text-white -mt-14">Lot Kosong</span>
                     </x-tab.title>
+
                     <x-tab.title name="8" livewire="wire:click=chooseCategory('W0')">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-6 h-6 ml-2">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
-                        </svg><span class="tooltip-text bg-blue-500 border rounded border-blue-500 text-white -mt-14">Waran</span>
+                        <x-heroicon-o-home class="w-6 h-6 ml-2"/>
+                        <span class="tooltip-text bg-blue-500 border rounded border-blue-500 text-white -mt-14">Waran</span>
                     </x-tab.title>
+
                     <x-tab.title name="9" livewire="wire:click=chooseCategory('T0')">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-6 h-6 ml-2">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01" />
-                        </svg><span class="tooltip-text bg-blue-500 border rounded border-blue-500 text-white -mt-14">Tempahan</span>
+                        <x-heroicon-o-clipboard-list class="w-6 h-6 ml-2"/>
+                        <span class="tooltip-text bg-blue-500 border rounded border-blue-500 text-white -mt-14">Tempahan</span>
                     </x-tab.title>
+
                     <x-tab.title name="10" livewire="wire:click=chooseCategory('BA')">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"class="w-6 h-6 ml-2">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 15a4 4 0 004 4h9a5 5 0 10-.1-9.999 5.002 5.002 0 10-9.78 2.096A4.001 4.001 0 003 15z" />
-                        </svg><span class="tooltip-text bg-blue-500 border rounded border-blue-500 text-white -mt-14">Bil Air</span>
+                        <x-heroicon-o-cloud class="w-6 h-6 ml-2"/>
+                        <span class="tooltip-text bg-blue-500 border rounded border-blue-500 text-white -mt-14">Bil Air</span>
                     </x-tab.title>
+
                     <x-tab.title name="11" livewire="wire:click=chooseCategory('BE')">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-6 h-6 ml-2">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z" />
-                        </svg><span class="tooltip-text bg-blue-500 border rounded border-blue-500 text-white -mt-14">Bil Elektrik</span>
+                        <x-heroicon-o-light-bulb class="w-6 h-6 ml-2"/>
+                        <span class="tooltip-text bg-blue-500 border rounded border-blue-500 text-white -mt-14">Bil Elektrik</span>
                     </x-tab.title>
+
                     <x-tab.title name="12" livewire="wire:click=chooseCategory('BS')">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-6 h-6 ml-2">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 12l3-3 3 3 4-4M8 21l4-4 4 4M3 4h18M4 4h16v12a1 1 0 01-1 1H5a1 1 0 01-1-1V4z" />
-                        </svg><span class="tooltip-text bg-blue-500 border rounded border-blue-500 text-white -mt-14">Bil Setempat PBT</span>
+                        <x-heroicon-o-presentation-chart-line class="w-6 h-6 ml-2"/>
+                        <span class="tooltip-text bg-blue-500 border rounded border-blue-500 text-white -mt-14">Bil Setempat PBT</span>
                     </x-tab.title>
+
                     <x-tab.title name="13" livewire="wire:click=chooseCategory('BC')">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-6 h-6 ml-2">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 12l3-3 3 3 4-4M8 21l4-4 4 4M3 4h18M4 4h16v12a1 1 0 01-1 1H5a1 1 0 01-1-1V4z" />
-                        </svg><span class="tooltip-text bg-blue-500 border rounded border-blue-500 text-white -mt-14">Bil Cukai Tanah</span>
+                        <x-heroicon-o-presentation-chart-line class="w-6 h-6 ml-2"/>
+                        <span class="tooltip-text bg-blue-500 border rounded border-blue-500 text-white -mt-14">Bil Cukai Tanah</span>
                     </x-tab.title>
+
                     <x-tab.title name="14" livewire="wire:click=chooseCategory('P1')">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"  class="w-6 h-6 ml-2">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4" />
-                        </svg><span class="tooltip-text bg-blue-500 border rounded border-blue-500 text-white -mt-14">Permit</span>
+                        <x-heroicon-o-newspaper class="w-6 h-6 ml-2"/>
+                        <span class="tooltip-text bg-blue-500 border rounded border-blue-500 text-white -mt-14">Permit</span>
                     </x-tab.title>
+
                 </div>
             </div>
         </div>
